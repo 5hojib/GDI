170169c71c9ebfefa97e8b3f4cdc0ad72fb11f72
5hojib <yesiamshojib@gmail.com>
2024-06-30 11:20:24 +0600

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/worker.js', // Adjust the path as per your actual file structure
+  entry: {
+    worker: './src/worker.js',
+    app: './src/app.js', // Add your app.js entry point
+  },
   output: {
-    filename: 'worker.js', // Output file name
+    filename: '[name].js', // Output file name based on entry point name
     path: path.resolve(__dirname, 'dist'), // Output directory
   },
   mode: 'production', // Set the mode to 'production' or 'development'
